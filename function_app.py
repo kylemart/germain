3cf03230d65c76d5cec7d55c17d7a2002c327285
Kyle Martinez <git@kylem.art>
2025-06-21 17:09:24 -0400

    Simplified function app

--- function_app.py
+++ function_app.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import logging
 
 import azure.functions as func
@@ -7,8 +6,8 @@ from icalendar import Calendar
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="normalize", methods=["GET"])
-def normalize(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="patch", methods=["GET"])
+def patch(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(f"Processing request.")
 
     url = req.params.get('ics_url')
@@ -25,41 +24,23 @@ def normalize(req: func.HttpRequest) -> func.HttpResponse:
             status_code=url_response.status_code
         )
     
-    calendar: Calendar = None
-    missing_tzids: set[str] = {}
+    calendar: Calendar | None = None
     try: 
         calendar = Calendar.from_ical(url_response.content)
-        if not calendar:
-            return func.HttpResponse(
-                "No calendar data found.",
-                status_code=400
-            )
-        
-        missing_tzids = calendar.get_missing_tzids()
-        if not missing_tzids:
-            return func.HttpResponse(
-                calendar.to_ical(),
-                mimetype='text/calendar'
-            )
-        
     except ValueError as e:
         logging.error(f"Failed to parse the calendar: {e}")
         return func.HttpResponse(
             f"Invalid calendar data at '{url}'.",
             status_code=400
         )
+    
+    if not calendar:
+        return func.HttpResponse(
+            "No calendar data found.",
+            status_code=400
+        )
 
-    for event in calendar.events:
-        patched = False
-        if event['DTSTART'].params.get('TZID') in missing_tzids:
-            event.DTSTART = event.DTSTART.astimezone(event.DTSTART.tzinfo)
-            patched = True
-        if event['DTEND'].params.get('TZID') in missing_tzids:
-            event.DTEND = event.DTEND.astimezone(event.DTEND.tzinfo)
-            patched = True
-        if patched:
-            logging.info(f"Patched event: uid='{event.uid}'.")
-
+    calendar.add_missing_timezones()
     return func.HttpResponse(
         calendar.to_ical(), 
         mimetype='text/calendar'
